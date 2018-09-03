246d5e5613621b5c4c183a998af29d545b110296
karunakarreddych <38121478+karunakarreddych@users.noreply.github.com>
2018-09-03 15:44:08 +0530

    Update index.py

--- index.py
+++ index.py
@@ -2,7 +2,7 @@ from flask import Flask
 app = Flask(__name__)
 @app.route("/")
 def hello():
-    return "Hello Wavelsdfdffabs!!!"
+    return "Welcome to Wavelabs Technologies!!!"
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=int("5000"), debug=True)
 Finished: SUCCESS
